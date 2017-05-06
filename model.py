53f2c31b9671719860b612bdf2e5f83200d285d5
cheng-PC\cheng <weicheng113@gmail.com>
2017-05-06 12:01:06 +1000

    experimenting with left and right images

--- model.py
+++ model.py
@@ -14,8 +14,42 @@ def load_data(data_dir):
     print("Loading data(driving log file: ", driving_log, ", image directory: ", image_dir, ")")
 
     # Format: Center Image, Left Image, Right Image, Steering Angle, Throttle, Break, Speed
+    raw_lines = read_driving_log(driving_log)
+    lines = map(lambda line: parse_driving_log_line(line=line, image_dir=image_dir), raw_lines)
+
+    images = []
+    steering_angles = []
+    for line in lines:
+        (center_image_path, left_image_path, right_image_path, center_steering_angle, throttle, _break, speed) = line
+
+        center_image = cv2.imread(center_image_path)
+        left_image = cv2.imread(left_image_path)
+        right_image = cv2.imread(right_image_path)
+        center_image_flipped = np.fliplr(center_image)
+
+        steering_correction_factor = 0.2 # this is a parameter to tune
+        left_steering_angle = center_steering_angle + steering_correction_factor
+        right_steering_angle = center_steering_angle - steering_correction_factor
+        center_steering_angle_flipped = -center_steering_angle
+
+        images.extend([center_image, left_image, right_image, center_image_flipped])
+        steering_angles.extend([center_steering_angle, left_steering_angle, right_steering_angle, center_steering_angle_flipped])
+
+    X_train = np.array(images)
+    y_train = np.array(steering_angles)
+    return X_train, y_train
+
+def parse_driving_log_line(line, image_dir):
+    # Format: Center Image, Left Image, Right Image, Steering Angle, Throttle, Break, Speed
+    [center_image_path, left_image_path, right_image_path, steering_angle, throttle, _break, speed] = line
+    def map_path(path):
+        filename = os.path.split(path)[-1]
+        return image_dir + "/" + filename
+    return (map_path(center_image_path), map_path(left_image_path), map_path(right_image_path), float(steering_angle), throttle, _break, speed)
+
+def read_driving_log(filename):
     lines = []
-    with open(driving_log) as csvfile:
+    with open(filename) as csvfile:
         has_header = csv.Sniffer().has_header(csvfile.read(1024))
         csvfile.seek(0)  # rewind
         reader = csv.reader(csvfile)
@@ -23,31 +57,12 @@ def load_data(data_dir):
             next(reader)  # skip header row
         for line in reader:
             lines.append(line)
-
-    images = []
-    steering_angles = []
-    for line in lines:
-        src_path = line[0]
-        filename = os.path.split(src_path)[-1]
-        file_path = image_dir + "/" + filename
-
-        image = cv2.imread(file_path, cv2.IMREAD_COLOR)
-        images.append(image)
-        steering_angle = float(line[3])
-        steering_angles.append(steering_angle)
-
-        image_flipped = np.fliplr(image)
-        images.append(image_flipped)
-        steering_angle_flipped = -steering_angle
-        steering_angles.append(steering_angle_flipped)
-    X_train = np.array(images)
-    y_train = np.array(steering_angles)
-    return X_train, y_train
+        return lines
 
 def LeNet(input_shape):
     model = Sequential()
 
-    model.add(Lambda(lambda x: x/255 - 0.5, input_shape=input_shape))
+    model.add(Lambda(preprocess, input_shape=input_shape))
 
     model.add(Conv2D(6, (5, 5)))
     model.add(MaxPooling2D((2, 2)))
@@ -64,17 +79,22 @@ def LeNet(input_shape):
 
     return model
 
-X_train, y_train = load_data("data")
-print("X_train shape: ", X_train.shape)
-print("y_train shape: ", y_train.shape)
-# Build model
-model = LeNet(input_shape=(160, 320, 3))
-model.compile(optimizer="adam", loss="mse")
-# Train model
-model.fit(x=X_train, y=y_train, batch_size=128, validation_split=0.3, shuffle=True, epochs=5)
-# Save model
-model_file = "model.h5"
-print("Saved model to: ", model_file)
+def preprocess(image):
+    return image/255 - 0.5
 
-# Temporary fix - AttributeError: 'NoneType' object has no attribute 'TF_NewStatus
-K.clear_session()
+if __name__ == '__main__':
+    X_train, y_train = load_data("data")
+    print("X_train shape: ", X_train.shape)
+    print("y_train shape: ", y_train.shape)
+    # Build model
+    # model = LeNet(input_shape=(160, 320, 3))
+    # model.compile(optimizer="adam", loss="mse")
+    # # Train model
+    # model.fit(x=X_train, y=y_train, batch_size=128, validation_split=0.3, shuffle=True, epochs=5)
+    # # Save model
+    # model_file = "model.h5"
+    # model.save(model_file)
+    # print("Saved model to: ", model_file)
+    #
+    # # Temporary fix - AttributeError: 'NoneType' object has no attribute 'TF_NewStatus
+    # K.clear_session()
--- model_v2.py
+++ model_v2.py
@@ -2,11 +2,10 @@ import numpy as np
 import csv
 import cv2
 import os.path
-import matplotlib.pyplot as plt
-import random
 # TODO: import Keras layers you need here
 from keras.models import Sequential
-from keras.layers.core import Dense, Flatten, Lambda
+from keras.layers.core import Dense, Flatten, Lambda, Activation
+from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras import backend as K
 
 def load_data(data_dir):
@@ -31,27 +30,36 @@ def load_data(data_dir):
         src_path = line[0]
         filename = os.path.split(src_path)[-1]
         file_path = image_dir + "/" + filename
+
         image = cv2.imread(file_path, cv2.IMREAD_COLOR)
         images.append(image)
-
         steering_angle = float(line[3])
         steering_angles.append(steering_angle)
-        #print("image: ", file_path, ", steering angle: ", steering_angle)
 
+        image_flipped = np.fliplr(image)
+        images.append(image_flipped)
+        steering_angle_flipped = -steering_angle
+        steering_angles.append(steering_angle_flipped)
     X_train = np.array(images)
     y_train = np.array(steering_angles)
     return X_train, y_train
-def random_visualize(images):
-    index = random.randint(0, len(images))
-    image = images[index]
-    plt.imshow(image)
-    plt.show()
 
-def LinearNet(input_shape):
+def LeNet(input_shape):
     model = Sequential()
 
     model.add(Lambda(preprocess, input_shape=input_shape))
+
+    model.add(Conv2D(6, (5, 5)))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Activation("relu"))
+
+    model.add(Conv2D(6, (5, 5)))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Activation("relu"))
+
     model.add(Flatten())
+    model.add(Dense(120))
+    model.add(Dense(84))
     model.add(Dense(1))
 
     return model
@@ -63,14 +71,13 @@ if __name__ == '__main__':
     X_train, y_train = load_data("data")
     print("X_train shape: ", X_train.shape)
     print("y_train shape: ", y_train.shape)
-    random_visualize(X_train)
     # Build model
-    model = LinearNet(input_shape=(160, 320, 3))
+    model = LeNet(input_shape=(160, 320, 3))
     model.compile(optimizer="adam", loss="mse")
     # Train model
-    model.fit(x=X_train, y=y_train, validation_split=0.3, shuffle=True, epochs=2)
+    model.fit(x=X_train, y=y_train, batch_size=128, validation_split=0.3, shuffle=True, epochs=5)
     # Save model
-    model_file = "model_v1.h5"
+    model_file = "model_v2.h5"
     model.save(model_file)
     print("Saved model to: ", model_file)
 
