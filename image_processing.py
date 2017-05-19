d1f0f5000b46e2aca882df570d16581da7740429
cheng-PC\cheng <weicheng113@gmail.com>
2017-05-19 23:22:38 +1000

    Updated model v8

--- image_processing.py
+++ image_processing.py
@@ -1,6 +1,30 @@
 import cv2
 import numpy as np
 
+def contrast_rgb_image(rgb_image):
+    #-----Converting image to LAB Color model-----------------------------------
+    lab = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2LAB)
+
+    #-----Splitting the LAB image to different channels-------------------------
+    l, a, b = cv2.split(lab)
+    #cv2.imshow('l_channel', l)
+    #cv2.imshow('a_channel', a)
+    #cv2.imshow('b_channel', b)
+
+    #-----Applying CLAHE to L-channel-------------------------------------------
+    clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
+    cl = clahe.apply(l)
+    #cv2.imshow('CLAHE output', cl)
+
+    #-----Merge the CLAHE enhanced L-channel with the a and b channel-----------
+    limg = cv2.merge((cl,a,b))
+    #cv2.imshow('limg', limg)
+
+    #-----Converting image from LAB Color model to RGB model--------------------
+    final = cv2.cvtColor(limg, cv2.COLOR_LAB2RGB)
+    #cv2.imshow('final', final)
+    return final
+
 def contrast_image(bgr_image):
     #-----Converting image to LAB Color model-----------------------------------
     lab = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2LAB)
